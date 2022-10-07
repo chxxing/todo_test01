f00f7906e9fa6717766a1c04611d6d81870acb6b
chxxing <choing26@naver.com>
2022-10-07 14:42:02 +0900

    221007

---
+++ src/board/List.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './board.scss';
+
+const List = ({ boardList, setBoardList }) => {
+    // const LocalList = JSON.parse(localStorage.getItem('list'));
+    return (
+        <div className='BoardList'>
+            <table className='BoardTable'>
+                <thead>
+                    <tr>
+                        <td className='no'>no</td>
+                        <td className='tit'>title</td>
+                        <td className='name'>name</td>
+                        <td className='date'>date</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        boardList.map((it, idx) => {
+                            return (
+                                <tr key={idx}>
+                                    <td className='no'>{idx + 1}</td>
+                                    <td className='tit'>
+                                        <Link to={'/view/' + it.id}>
+                                            {it.title}
+                                        </Link>
+                                    </td>
+                                    <td className='name'>{it.name}</td>
+                                    <td className='date'>{it.date}</td>
+                                    {/* .replaceAll('.', '') */}
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+            <div className="BtnGroup">
+                <Link to='/write'>
+                    <button>write</button>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default List;
--- src/todo_basic02.js
+++ src/todo_basic02.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const App = () => {
   const [todo, setTodo] = useState({});
@@ -7,14 +7,15 @@ const App = () => {
     <div>
       <ul>
         {
-          todolist.map((it, idx) => <li key={idx}>{it.title} / {it.content}</li>)
+          todolist.map((it, idx) => <li key={idx}>{it.title}  / {it.content}</li>)
         }
       </ul>
       <input onChange={e => setTodo({ ...todo, [e.target.name]: e.target.value })} name='title' />
       <input onChange={e => setTodo({ ...todo, [e.target.name]: e.target.value })} name='content' />
       <button onClick={() => setTodolist([...todolist, todo])}>WRITE</button>
+      {console.log(todo)}
     </div>
   )
 }
 
-export default App;
+export default App
